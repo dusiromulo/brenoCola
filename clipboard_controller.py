b58aeb0394d79eb429732f9ba876ca93dd316b78
dusiromulo <dusiromulo@gmail.com>
2016-10-27 01:03:13 -0200

    refactoring [1/2]: removing files

--- clipboard_controller.py
+++
@@ -1,5 +0,0 @@
-
-from os_identifier import OsIdentifier
-from global_shortcut_manager import GlobalShortcutManager
-
-GlobalShortcutManager(OsIdentifier().get_os_type()).set_service_on()
--- main_controller.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Roland'
-
-
-class MainController():
-    pass
